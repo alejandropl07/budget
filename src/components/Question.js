c28bc2fdf55b16d03dc927c1f8ab51fa5e3a8dd3
alejandropl07 <aprietoleon97@gmail.com>
2022-05-29 19:30:04 -0500

    Validated budget

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 function Question() {
+  const [amount, saveAmount] = useState(0);
+  const [error, saveError] = useState(false);
+
+  const addBudget = (e) => {
+    e.preventDefault();
+    if (amount < 1 || isNaN(amount)) {
+      saveError(true);
+      return;
+    }
+  };
+
   return (
     <Fragment>
       <h2>Place your budget</h2>
-      <form>
+      {error ? (
+        <p className="alert    alert-danger    error">Incorrect budget</p>
+      ) : null}
+      <form onSubmit={addBudget}>
         <input
           type="number"
           name="budget"
           id="budget"
           placeholder="Add budget"
           className="u-full-width"
-          //onChange={d}
+          onChange={(e) => saveAmount(parseInt(e.target.value, 10))}
         />
-        <input type="submit"    className="button-primary   u-full-width" value="Add budget" />
+        <input
+          type="submit"
+          className="button-primary   u-full-width"
+          value="Add budget"
+        />
       </form>
     </Fragment>
   );
